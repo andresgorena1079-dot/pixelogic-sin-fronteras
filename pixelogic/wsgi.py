9155a1e3323bbfba6e176fa0a8c4eac94ee02cdb
Carlos Quezada <andresgorena1079@gmail.com>
2025-09-22 16:03:06 -0300

    Corregir settings para usar un único archivo de configuración

--- pixelogic/wsgi.py
+++ pixelogic/wsgi.py
@@ -13,6 +13,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Cambiamos 'pixelogic.settings' por 'pixelogic.prod_settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pixelogic.prod_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pixelogic.settings")
 
 application = get_wsgi_application()
