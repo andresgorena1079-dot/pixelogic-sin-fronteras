a93457b66cb65b098dda7a115ae75c13bd0eafe7
Carlos Quezada <andresgorena1079@gmail.com>
2025-09-22 12:17:20 -0300

    Implementar configuración de producción separada

---
+++ pixelogic/prod_settings.py
@@ -0,0 +1,24 @@
+# pixelogic/prod_settings.py
+import os
+
+import dj_database_url
+
+from .settings import *
+
+DEBUG = False
+
+# Configuración de la URL de Render
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Configuración de la base de datos de Render
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# Configuración de WhiteNoise para archivos estáticos
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
--- pixelogic/wsgi.py
+++ pixelogic/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for pixelogic project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+# pixelogic/wsgi.py
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixelogic.settings')
+# Cambiamos 'pixelogic.settings' por 'pixelogic.prod_settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pixelogic.prod_settings")
 
 application = get_wsgi_application()
