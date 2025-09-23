f6dd87f173509152ab885e88622e01e22b2aa3a0
Carlos Quezada <andresgorena1079@gmail.com>
2025-09-23 10:30:10 -0300

    Make build.sh executable for Render deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-# exit on error
+#!/bin/bash
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
