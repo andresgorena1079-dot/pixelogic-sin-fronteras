91da975c970295ac7ab2d85655c1bec9c33b8964
Carlos Quezada <andresgorena1079@gmail.com>
2025-09-23 12:42:58 -0300

    Crea superusuario automáticamente en Render

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py loaddata db.json
+python create_superuser.py
+python manage.py migrate
+python manage.py shell
