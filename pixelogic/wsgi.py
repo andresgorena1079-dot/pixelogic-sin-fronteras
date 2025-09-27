79589bee17a070a42d9a15a89c26051665b0a63f
Carlos Quezada <andresgorena1079@gmail.com>
2025-09-27 02:27:26 -0300

    Limpia wsgi.py y prepara para carga de datos en Build Command

--- pixelogic/wsgi.py
+++ pixelogic/wsgi.py
@@ -5,8 +5,3 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pixelogic.settings")
 application = get_wsgi_application()
-
-# --- ACTIVADO PARA LA CARGA DE DATOS EN RENDER ---
-# IMPORTANTE: Comentar esta línea después de la carga exitosa.
-if os.environ.get("RENDER"):
-    exec(open("startup.py").read())
